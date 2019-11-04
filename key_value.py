5aba18dd42230a33991fafd0126268e61ca9e065
Suzanne Berger <tangle1@mindspring.com>
2019-11-04 12:58:18 -0800

    new Maya script key_value.py

---
+++ key_value.py
@@ -0,0 +1,53 @@
+#!/bin/env python
+import sys
+import os
+
+from pprint import pprint
+
+from maya import cmds
+
+import scnutils
+
+def getKeyValues(nodeAttr, startFrame, endFrame):
+    keys = cmds.keyframe(nodeAttr, time=(startFrame, endFrame), query=True, valueChange=True, timeChange=True)
+    key_values = []
+    if keys:
+        key_values = [(keys[i], keys[i+1]) for i in range(0,len(keys),2)]
+    return key_values
+
+def allKeyValues():
+    startFrame = cmds.playbackOptions(q=True,animationStartTime=True)
+    endFrame = cmds.playbackOptions(q=True,animationEndTime = True)
+    keyval_dict = {}
+
+    anim_list = cmds.ls(type="animCurve")
+    attr_list = []
+    for each in anim_list:
+        node_attrs = cmds.listConnections(each, p=True, s=True)
+        attr_list.append(node_attrs[0])
+
+    for nodeAttr in attr_list:
+        key_values = getKeyValues(nodeAttr, startFrame, endFrame)
+        keyval_dict[nodeAttr] = key_values
+
+    return keyval_dict
+
+
+if __name__ == '__main__':
+
+    print("main key_value")
+    if scnutils.isStandalone():
+        print("Running Maya standalone")
+        
+        if len(sys.argv) < 2:
+            print("Error: Missing input scene file argument")
+            sys.exit(1)
+
+        scenefile = sys.argv[1]
+        print("Loading input Maya scene file")
+        scnutils.openScene(scenefile)
+        key_value_dict = allKeyValues()
+        pprint(key_value_dict)
+
+
+    sys.exit(0)
