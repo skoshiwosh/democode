80c23627214bdb46432c80a116e771a7dfe37f16
Suzanne Berger <tangle1@mindspring.com>
2019-11-30 20:37:28 -0800

    removed dependency on scnutils

--- key_value.py
+++ key_value.py
@@ -1,12 +1,9 @@
 #!/bin/env python
 import sys
 import os
-
 from pprint import pprint
 
 from maya import cmds
-
-import scnutils
 
 def getKeyValues(nodeAttr, startFrame, endFrame):
     keys = cmds.keyframe(nodeAttr, time=(startFrame, endFrame), query=True, valueChange=True, timeChange=True)
@@ -29,14 +26,19 @@ def allKeyValues():
     for nodeAttr in attr_list:
         key_values = getKeyValues(nodeAttr, startFrame, endFrame)
         keyval_dict[nodeAttr] = key_values
-
     return keyval_dict
 
 
 if __name__ == '__main__':
+    
+    try:
+        import maya.standalone
+        maya.standalone.initialize(name='python')
+        isStandalone = True
+    except:
+        isStandalone = False
 
-    print("main key_value")
-    if scnutils.isStandalone():
+    if isStandalone:
         print("Running Maya standalone")
         
         if len(sys.argv) < 2:
@@ -44,10 +46,14 @@ if __name__ == '__main__':
             sys.exit(1)
 
         scenefile = sys.argv[1]
-        print("Loading input Maya scene file")
-        scnutils.openScene(scenefile)
+        print("Loading input Maya scene file", scenefile)
+        try:
+            cmds.file(scenefile, iv=True, force=True, open=True)
+        except:
+            print("Error: Unable to open Maya scene file")
+            sys.exit(1)
+        
         key_value_dict = allKeyValues()
         pprint(key_value_dict)
-
 
     sys.exit(0)
