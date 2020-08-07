9435a6a7a1c3c5ac6b2b3b5e5c379b8446609cfb
Suzanne Berger <tangle1@mindspring.com>
2020-08-07 16:13:00 -0700

    gui now works for new json

--- clientgui.py
+++ clientgui.py
@@ -36,6 +36,9 @@ class InvoiceWin(QtWidgets.QDialog):
     def __init__(self, parent=None):
         QtWidgets.QDialog.__init__(self, parent)
 
+        self.newjson = False
+        self.client = cinv.ClientInvoice()
+        
         self._initUI()
         self._connectSignals()
         self.show()
@@ -44,11 +47,11 @@ class InvoiceWin(QtWidgets.QDialog):
         self.setGeometry(100, 100, 600, 400)
         self.setWindowTitle('Client Invoice')
     
-        self.name_label = QtWidgets.QLabel("Client Name: ")
+        self.name_label = QtWidgets.QLabel("Client Name:  ")
         self.name_label.setFixedSize(81,21)
         self.name_lineEdit = QtWidgets.QLineEdit()
         self.name_lineEdit.setFixedHeight(21)
-        self.name_lineEdit.setMinimumWidth(151)
+        self.name_lineEdit.setMinimumWidth(101)
         self.load_pushButton = QtWidgets.QPushButton("Load Invoice")
         self.load_pushButton.setFixedSize(121, 35)
         self.new_pushButton = QtWidgets.QPushButton("New Invoice")
@@ -60,21 +63,31 @@ class InvoiceWin(QtWidgets.QDialog):
         head_layout.addWidget(self.load_pushButton)
         head_layout.addWidget(self.new_pushButton)
     
-        self.file_label = QtWidgets.QLabel("Client Invoice File: ")
-        self.file_label.setFixedSize(101,21)
+        self.address_label = QtWidgets.QLabel("Client Address:  ")
+        self.address_label.setFixedSize(101,21)
+        self.address_lineEdit = QtWidgets.QLineEdit()
+        self.address_lineEdit.setFixedHeight(21)
+        self.address_lineEdit.setMinimumWidth(101)
+        
+        address_layout = QtWidgets.QHBoxLayout()
+        address_layout.addWidget(self.address_label)
+        address_layout.addWidget(self.address_lineEdit)
+    
+        self.file_label = QtWidgets.QLabel("Client Invoice File:  ")
+        self.file_label.setFixedSize(121,21)
         self.file_lineEdit = QtWidgets.QLineEdit()
         self.file_lineEdit.setFixedHeight(21)
-        self.name_lineEdit.setMinimumWidth(301)
+        self.name_lineEdit.setMinimumWidth(201)
         
         file_layout = QtWidgets.QHBoxLayout()
         file_layout.addWidget(self.file_label)
         file_layout.addWidget(self.file_lineEdit)
     
-        status_label = QtWidgets.QLabel('Status')
+        status_label = QtWidgets.QLabel("Status:  ")
         status_label.setFixedSize(61,21)
         self.status_lineEdit = QtWidgets.QLineEdit("Ready:")
         self.status_lineEdit.setFixedHeight(21)
-        self.status_lineEdit.setMinimumWidth(600)
+        self.status_lineEdit.setMinimumWidth(580)
         
         status_layout = QtWidgets.QHBoxLayout()
         status_layout.addWidget(status_label)
@@ -87,7 +100,10 @@ class InvoiceWin(QtWidgets.QDialog):
         
         mainLayout = QtWidgets.QVBoxLayout()
         mainLayout.addLayout(head_layout)
+        mainLayout.addLayout(address_layout)
+        mainLayout.addStretch(31)
         mainLayout.addLayout(file_layout)
+        mainLayout.addStretch(3)
         mainLayout.addLayout(status_layout)
         mainLayout.addWidget(self.buttonbox)
         self.setLayout(mainLayout)
@@ -104,18 +120,46 @@ class InvoiceWin(QtWidgets.QDialog):
         event.accept()
 
     def save(self):
-        logging.info("Save clicked")
         #print("Save clicked")
+        logging.info("Save clicked")
+        jsonfile = None
+        if self.newjson:
+            client_name = self.name_lineEdit.text()
+            client_address = self.address_lineEdit.text()
+            jsonfile = self.client.newjson(client_name,
+                                    client_address=client_address,
+                                    jsonfile=self.jsondir)
+        else:
+            logging.info("Updating existing json not yet implemented")
+            self.status_lineEdit.setText("Updating existing json not yet implemented")
+        
+        if jsonfile:
+            self.file_lineEdit.setText(jsonfile)
+            self.status_lineEdit.setText("Created new client invoice json file")
+
 
     def reset(self):
         logging.info("Resetting Client Invoice window")
+        self.newjson = False
+        # delete any exisiting client object and create new client object
+        self.name_lineEdit.clear()
+        self.address_lineEdit.clear()
+        self.file_lineEdit.clear()
+        self.status_lineEdit.setText("Ready: ")
 
     def on_load_clicked(self):
         logging.info("Load button clicked")
 
     def on_new_clicked(self):
-        logging.info("new button clicked")
+        logging.info("New button clicked")
+        self.jsondir = QtWidgets.QFileDialog.getExistingDirectory(self,
+                                'Select Directory',
+                                '/Users/suzanneberger/Documents/teaching/terence',
+                                QtWidgets.QFileDialog.ShowDirsOnly | QtWidgets.QFileDialog.DontResolveSymlinks)
 
+        self.file_lineEdit.setText(self.jsondir)
+        self.newjson = True
+        return
 
 
 
