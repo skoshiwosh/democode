ee4ce97fae15a54863d064aaa2a8c00ac3c704e3
Suzanne Berger <tangle1@mindspring.com>
2020-09-21 14:24:51 -0700

    added comments and docstrings

--- demoscript.py
+++ demoscript.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-"""
-    Example python script used for teaching.
-    
+""" Example python script used for teaching.
+
     File: demoscript.py
-    Version: V02
-    Date: Sept 24, 2019
+    Version: 1.0
+    Date: Sept 21, 2020
     Author: Suzanne Berger
-    Contact: zanefx7@gmail.com       
+    Contact: zanefx7@gmail.com
 """
 
 import sys
@@ -19,15 +18,20 @@ import logging
 
 VERSION = "V02"
 logging.basicConfig(level=logging.INFO)
-logging.info("%s Version %s" % (__file__, VERSION))
+logging.info("%s Version %s" % (__file__, VERSION))     # note this is old-style string formating
 
+print("__name__ is this: %s" % __name__)
 #print("cmp_ints is type ", type(cmp_ints))     # will error because 'cmp_ints' not defined yet
 
 #########################################################
 # methods
 #########################################################
 
+# this logic demostrates if/elif/else
+# same list is also returned by one line statement: range(min(arg2,arg2), max(arg1,arg2)+1)
 def cmp_ints(arg1, arg2):
+    """Return list of numbers between parameters arg1 and arg2 sorted min to max."""
+    
     if arg1 > arg2:
         print("cmp_ints: arg1: {} is bigger".format(arg1))
         return range(arg2, arg1)
@@ -38,9 +42,14 @@ def cmp_ints(arg1, arg2):
         print("cmp_ints: arg1: {} and arg2: {} are equal".format(arg1, arg2))
         return [arg1]
 
-print("cmp_ints is type ", type(cmp_ints))
+print("cmp_ints is type:", type(cmp_ints))      # object type
+print("cmp_ints id is:", id(cmp_ints))          # object memory address
 
+# this logic demonstrates while loop
+# same list is also returned by list comprehension: [i**2 for i in range(max_int,0,-1)]
 def get_powers(max_int):
+    """Return list of numbers powered by 2 starting at max_int parameter."""
+    
     powers = []                     # start with empty list
     looper = max_int
     while looper > 0:
@@ -57,13 +66,13 @@ print("get_powers is type ", type(get_powers))
 
 if __name__ == "__main__":
 
-    print("sys.argv list: ", sys.argv)
+    print("sys.argv list:", sys.argv)
     if len(sys.argv) < 3:
         logging.error(" {} is missing arguments".format(sys.argv[0]))
         sys.exit(1)
-
-    arg1 = sys.argv[1]
-    arg2 = sys.argv[2]
+    else:
+        arg1 = sys.argv[1]
+        arg2 = sys.argv[2]
 
     try:
         i = int(arg1)
@@ -78,7 +87,7 @@ if __name__ == "__main__":
         print(each)
 
     powers = get_powers(max(i, j))      # use built-in max function to pass in max integer
-    print("powers ", powers)
+    print("powers: {}".format(powers))
 
     # exiting and all is good       
     sys.exit()
