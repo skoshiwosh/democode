4e559a4a744815e147ef8ba2fffa7afeb7d4e7f6
Suzanne Berger <tangle1@mindspring.com>
2025-04-15 10:50:18 -0700

    Add files via upload

---
+++ clientwin.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#  clientwin.sh
+#
+#
+#  Created by Suzanne Berger on 11/30/17.
+#
+#MAYAPY=/Applications/Autodesk/maya2018/Maya.app/Contents/bin/mayapy
+CLIWIN=/Users/suzanneberger/Documents/teaching/terence/clientgui.py
+#
+#$MAYAPY $CLIWIN $*
+python3 $CLIWIN $*
+
